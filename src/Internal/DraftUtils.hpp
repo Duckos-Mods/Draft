15c430e4db4775d1337b8995cb068c7ed77f9afc
Duckos-Mods <businwes@hotmail.com>
2024-03-21 21:34:31 +0000

    le goober

--- src/Internal/DraftUtils.hpp
+++ src/Internal/DraftUtils.hpp
@@ -51,8 +51,19 @@ namespace Draft
 
 
 	template <typename FunctionPtr>
-	void* LocalFunctionPointerToRawAddress(FunctionPtr ptr)
+	void* LocalFunctionPointerToRawAddress(FunctionPtr func)
 	{
-		return *reinterpret_cast<void**>(&ptr);
+
+		void* pFunction = *reinterpret_cast<void**>(&func);
+
+		char* ptr = reinterpret_cast<char*>(pFunction);
+		ptr++;
+		int32_t offset = *reinterpret_cast<int32_t*>(ptr);
+		ptr--;
+		uint64_t target = ((uint64_t)ptr + offset);
+		while (target % 16 != 0) {
+			target++;
+		}
+		return (void*)target;
 	}
 }
