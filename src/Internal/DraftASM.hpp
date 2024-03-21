1dcecd8d6de51a54b58b98d2742d36185852cefc
Duckos-Mods <businwes@hotmail.com>
2024-03-21 17:33:49 +0000

    Added Utility

--- src/Internal/DraftASM.hpp
+++ src/Internal/DraftASM.hpp
@@ -1,59 +1,11 @@
 #pragma once
 #include "DraftUtils.hpp"
+#include "DraftASMTypes.hpp"
 #include <array>
 #include <span>
 
 namespace Draft
 {
-    enum class Opcodes : uint8_t
-    {
-        RET = 0xC3,
-        MOV = 0xC7,
-        PUSH = 0x68,
-        JMP = 0xE9,
-
-
-        JMP64_MOV_MODRM = 0x44,
-        JMP64_MOV_SIB = 0x24,
-        JMP64_MOV_OFFSET = 0x4,
-    };
-
-    uint8_t constexpr OpToByte(Opcodes op)
-    {
-		return static_cast<uint8_t>(op);
-	}
-
-#pragma pack(push, 1)
-    struct jmp32 {
-        uint8_t opcode = OpToByte(Opcodes::JMP);
-        uint32_t offset;
-
-        jmp32(int32_t _offset) {
-            offset = _offset;
-		}
-    };
-
-
-
-    struct jmp64 {
-        uint8_t  pushOpcode = OpToByte(Opcodes::PUSH);
-        uint32_t pushLowAddressSlice = 0;
-        uint8_t  movOpcode = OpToByte(Opcodes::MOV);
-        uint8_t  movModRm = OpToByte(Opcodes::JMP64_MOV_MODRM);
-        uint8_t  movSib = OpToByte(Opcodes::JMP64_MOV_SIB);
-        uint8_t  movOffset = OpToByte(Opcodes::JMP64_MOV_OFFSET);
-        uint32_t movHighAddressSlice = 0;
-        uint8_t  retOpcode = OpToByte(Opcodes::RET);
-
-        jmp64(uintptr_t address) {
-            pushLowAddressSlice = static_cast<uint32_t>(address & 0xFFFFFFFF);
-            movHighAddressSlice = static_cast<uint32_t>((address >> 32) & 0xFFFFFFFF);
-        }
-
-    };
-
-#pragma pack(pop)
-
     class ASMHandler
     {
     public:
@@ -74,7 +26,8 @@ namespace Draft
 			Write(reinterpret_cast<uint8_t*>(&value), sizeof(T));
         }
 
-        size_t disassemble(std::span<uint8_t> code, size_t minSize);
+        size_t GetMinInstrSizeForMinsize(void* address, size_t minSize);
+        size_t GetInstrSize(void* address);
 
 
          size_t tellg() const { return m_offset; }
