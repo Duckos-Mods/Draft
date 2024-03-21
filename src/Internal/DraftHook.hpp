61299a87a78999b9a92f289a795f4f046d3dfac3
Duckos-Mods <businwes@hotmail.com>
2024-03-21 17:32:57 +0000

    Redid hooks and added trampoline generation

--- src/Internal/DraftHook.hpp
+++ src/Internal/DraftHook.hpp
@@ -13,11 +13,14 @@ namespace Draft
         // Automatically chooses the best method for the target function this can be slower than choosing the method yourself due to it all 2 times and comparing addresses
         JumpAutomaticInstall, 
 
-        // Installs a jump to the target function with 20 bytes
-        Jump20byteInstall,
+        // Installs a ret to the target function with 20 bytes. This is only here for completeness and is not recommended to be used. The 14 byte install method is faster
+        Ret20byteInstall = 20,
+
+        // Installs a jump to the target function with 14 bytes
+        Jump14byteInstall = 14,
 
         // Installs a jump to the target function with 5 bytes
-        Jump5byteInstall,
+        Jump5byteInstall = 5,
 
         InstallMethodCount
     };
@@ -39,6 +42,33 @@ namespace Draft
         void* m_function = nullptr;
         size_t m_size = -1;
     };
+
+
+    /**
+    * @brief This function attempts to take any function pointer and make a new function that calls it that is close to the requested address
+    * @brief This is useful for when you have a 6 byte function you need to hook so you have to use a 5 byte jump but the target function is too far away
+    * @param targetAddress The address of the target function
+    * @param function The function to call
+    * @param alignment The alignment of the memory to allocate
+    * @return The address of the new function that calls the target function
+    * @note This function is not thread safe and should only be called once per target function
+    * @example
+    * @code
+    * void* targetFunction = reinterpret_cast<void*>(0x12345678);
+    * void* newFunction = NearAddressFunction(targetFunction, &MyFunction);
+    * @endcode
+    */
+    template<typename FuncPtrType>
+    void* NearAddressFunction(void* targetAddress, FuncPtrType function, size_t alignment = 0x10) {
+        void* ptr = RawAlineAlloc<uint8_t>(EnumToUnderlying(HookInstallMethod::Jump14byteInstall), alignment);
+        DRAFT_THROW_IF(ptr == nullptr, "Failed to allocate memory");
+        void* funcPtr = *reinterpret_cast<void**>(&function);
+        jmp64 jmp(reinterpret_cast<uint64_t>(funcPtr);
+        ASMHandler handler(ptr, EnumToUnderlying(HookInstallMethod::Jump14byteInstall));
+        handler.Write(jmp);
+        SetProtection(ptr, EnumToUnderlying(HookInstallMethod::Jump14byteInstall), MemoryProtection::Execute);
+        return ptr;
+    }
 
     /**
     * @brief This class is thread safe if the hooked function is thread safe. Although calling uninstall from multiple threads at the same time is undefined behavior
@@ -62,33 +92,61 @@ namespace Draft
             size_t alignment = 0x10
         );
 
+        template<typename funcPtrType>
+        bool Install(
+			funcPtrType targetFunction,
+            funcPtrType hookFunction,
+			HookInstallMethod installMethod = HookInstallMethod::JumpAutomaticInstall,
+			AllocationMethod allocationMethod = AllocationMethod::FarAlloc,
+			size_t alignment = 0x10
+		) {
+			return Install(*reinterpret_cast<UnkfuncPtr*>(&targetFunction), *reinterpret_cast<UnkfuncPtr*>(&hookFunction), installMethod, allocationMethod, alignment);
+		}
+
 
         /**
         * @brief Uninstalls the hook
         */
-        void Uninstall() {
-            if (m_location != nullptr)
-            {
-                memcpy(m_location, m_originalFunction.m_function, m_originalFunction.m_size);
-                OsFree(m_originalFunction.m_function, m_originalFunction.m_size); // Free the copied function
-			}
-        }
+        void Uninstall();
 
         bool IsInstalled() const {
 			return m_isInstalled;
 		}
+
+        template<typename T, typename... Args>
+        T fastCall(Args... args) {
+            auto funcPtr = reinterpret_cast<T(__fastcall *)(Args...)>(m_trampoline);
+            return funcPtr(args...);
+		}
+
+		template<typename T, typename... Args>
+		T stdCall(Args... args) {
+			auto funcPtr = reinterpret_cast<T(__stdcall *)(Args...)>(m_trampoline);
+			return funcPtr(args...);
+        }
+
+        template<typename T, typename... Args>
+		T cdeclCall(Args... args) {
+            auto funcPtr = reinterpret_cast<T(__cdecl *)(Args...)>(m_trampoline);
+            return funcPtr(args...);
+        }
+
+        template<typename T, typename... Args>
+		T thisCall(Args... args) {
+			auto funcPtr = reinterpret_cast<T(__thiscall *)(Args...)>(m_trampoline);
+			return funcPtr(args...);
+		}
+
+
+
     private:
         bool FindInstallMethod(UnkfuncPtr targetLocation, HookInstallMethod& installMethod, UnkfuncPtr hookFunction);
-        bool GenerateTrampoline(UnkfuncPtr targetFunction, AllocationMethod allocationMethod, size_t alignment, size_t bytesToAllocate);
+        bool GenerateTrampoline(UnkfuncPtr targetFunction, AllocationMethod allocationMethod, size_t alignment, HookInstallMethod installMethod);
         bool IsBigEnough(UnkfuncPtr targetFunction, size_t size);
     private:
         void* m_location = nullptr; // The location of the function we are hooking
         void* m_trampoline = nullptr; // Used to call the original function head that we overwrote then jump back to the original function
         UnkownFunctionData m_originalFunction; // The original function data
         bool m_isInstalled = false;
-
-
-
-
     };
 }
